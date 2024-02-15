bf7b73bf3f15142a33207ed0cf0553d885ddbfad
bengetch <bengetch@gmail.com>
2024-02-15 10:22:56 -0500

    Add separate collector service to centralize event storage

--- src/entrypoint_service/start.sh
+++ src/entrypoint_service/start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 exec opentelemetry-instrument \
-  --traces_exporter console \
-  --metrics_exporter console \
-  --logs_exporter console \
+  --traces_exporter otlp \
+  --metrics_exporter otlp \
+  --logs_exporter otlp \
   --service_name entrypoint_service \
   flask run
